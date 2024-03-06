a725e4623bd1cedacf441de9564ba683191fbb75
vlado <vladocavric@gmail.com>
2024-03-06 18:32:43 +0100

    getStaticProps

---
+++ nextjs-course-code-04-data-fetching-starting-project/pages/index.js
@@ -0,0 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+export default function HomePage(props) {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((product) => (<li key={product.id}>{product.title}</li>))}
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products
+    }, 
+    revalidate: 60
+  };
+}
